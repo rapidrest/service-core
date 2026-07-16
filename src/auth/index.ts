9ef5034dac1cb632c6bd07a8121c6892f95ae933
Jean-Philippe Steinmetz <caskater47@gmail.com>
2026-07-16 09:07:00 -0700

    Adding new auth strategy that implemetns WebAuthN Passkeys

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -3,4 +3,5 @@ export * from "./AuthStrategy.js";
 export * from "./BasicStrategy.js";
 export * from "./JWTStrategy.js";
 export * from "./OAuthStrategy.js";
+export * from "./PasskeyStrategy.js";
 export * from "./TOTPStrategy.js";
